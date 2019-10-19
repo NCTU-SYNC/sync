58267fca6940f188bdc01e4e040cb0845dd8af3e
maxam2017 <whk0313@gmail.com>
2019-10-19 18:00:28 +0800

    Chore(editor): install dependency

--- src/modules/common/component/Editor.tsx
+++ src/modules/common/component/Editor.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Editor } from 'slate-react';
-
+import React, { useState } from 'react';
+import { Editor, OnChangeFn } from 'slate-react';
 import Plain from 'slate-plain-serializer';
+
 import styled from 'styled-components';
+import { Value } from 'slate';
 
 const StyledEditor = styled(Editor)`
   padding: 10px;
 `;
 
 const CustomEditor = ({ ...props }) => {
-  const initialValue = Plain.deserialize('Hello World');
+  const initialValue = Plain.deserialize('');
+  const [ value, setValue ] = useState<Value>(initialValue);
+
+  const handleChange: OnChangeFn = ({ value }) => {
+    setValue(value as Value);
+  };
+
   return (
-    <StyledEditor value={initialValue} {...props}/>
+    <StyledEditor value={value} onChange={handleChange} {...props}/>
   );
 };
 
