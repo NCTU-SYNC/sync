ce3d597913bb37168ea46bf593dd04476c5db75b
maxam2017 <whk0313@gmail.com>
2019-12-02 02:18:59 +0800

    feat(input): submit when press Enter

--- src/modules/common/component/Input.tsx
+++ src/modules/common/component/Input.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 interface IProps {
   className?: string;
   placeholder?: string;
   value?: string;
+  onSumbit?: (value: string) => void;
 }
 
 interface IGroupProps extends IProps {
@@ -42,13 +43,37 @@ export const Input = (props: IProps) => (
 );
 
 export const InputGroup = React.forwardRef<HTMLInputElement, IGroupProps>(
-  ({ className, left, right, ...props }: IGroupProps, ref) => (
-    <Main className={className}>
-      {left}
-      <InputInGroup ref={ref} {...props}/>
-      {right}
-    </Main>
-  )
+  ({ className, left, right, onSumbit, ...props }: IGroupProps, ref: any) => {
+    const handleSubmit = () => {
+      onSumbit && onSumbit(ref.current.value);
+    };
+
+    const handleReturn = (event: KeyboardEvent) => {
+      if (event.key !== 'Enter'){
+        return;
+      }
+      handleSubmit();
+    };
+
+    useEffect(() => {
+      if (ref && ref.current) {
+        ref.current.addEventListener('keydown', handleReturn);
+      }
+      return () => {
+        if (ref && ref.current) {
+          ref.current.removeEventListener('keydown', handleReturn);
+        }
+      };
+    }, []);
+
+    return (
+      <Main className={className}>
+        {left}
+        <InputInGroup ref={ref} {...props}/>
+        <div onClick={handleSubmit}>{right}</div>
+      </Main>
+    );
+  }
 );
 
 InputGroup.displayName = 'InputGroup';
