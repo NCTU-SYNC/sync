c1d9b9e63e6a8ee73afb98c55ccb6db7f895d0d4
maxam2017 <whk0313@gmail.com>
2020-03-06 03:13:11 +0800

    chore: make file cleaner

--- src/modules/post/component/PostCarousel.tsx
+++ src/modules/post/component/PostCarousel.tsx
@@ -163,10 +163,10 @@ class SlideGroup extends React.Component<ISlideGroupProps, ISlideGroupState> {
     this.state = {
       currentImageIndex: 2,
       images: [
-        '/static/avatar2.jpg',
-        '/static/123.jpg',
-        '/static/456.jpg',
-        '/static/avatar2.jpg',
+        '/public/avatar2.jpg',
+        '/public/123.jpg',
+        '/public/456.jpg',
+        '/public/avatar2.jpg',
         'https://picsum.photos/500/400?random=5',
         'https://picsum.photos/500/400?random=6',
       ],
@@ -200,7 +200,7 @@ class SlideGroup extends React.Component<ISlideGroupProps, ISlideGroupState> {
           </SlideBetween>
         );
       }
-      
+
       count = count + 1;
     }
     return slideItemList;
